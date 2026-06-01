542b8bf1027c1e8e08d7e436a4af6f8ed26126a4
TheNeovimmer <ilyesbouzayen@hotmail.com>
2026-06-01 22:28:41 +0100

    Update database connection settings in config.php

--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 
-define('DB_HOST', 'db');
+define('DB_HOST', 'localhost');
 define('DB_PORT', '3306');
 define('DB_NAME', 'hkservice');
 define('DB_USER', 'root');
-define('DB_PASS', 'root');
+define('DB_PASS', '');
 define('UPLOAD_DIR', __DIR__ . '/../uploads/');
